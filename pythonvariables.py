119461e07fdc318ed62f04bc8fd2b417828e1980
moruri <moruricharles63@gmail.com>
2023-12-21 23:32:02 +1400

    first commit

---
+++ pythonvariables.py
@@ -0,0 +1,61 @@
+#variables
+num1 = 39
+num2 =22
+sum = num1 + num2
+print(sum)
+
+print("Hello world")
+
+a, b, c= 5, 3.2, "hello"
+print (a)
+print(b)
+print(c)
+
+site1=site2="power learn project"
+print(site1)
+print(site2)
+
+#int
+me =3
+print(me , 'is of type',type(me))
+#strings
+Us="We were on the verge of greatness we were this close"
+print(Us)
+print(type(Us))
+
+#List
+Languages=["python","java","c++","ruby"]
+print(Languages[3],Languages[2],Languages[0],Languages[1])
+
+
+#set 
+student_id={5,6,7,8,3,2,1,9}
+print(student_id)
+print(type(student_id))
+
+
+
+int_number= 132
+float_number= 123.43
+new_num = int_number + float_number
+print("Value :" ,new_num)
+print(" Data type :", type(new_num))
+
+
+num_string ="123"
+num_int= 22
+
+num_string = int(num_string)
+sum= num_string + num_int
+print("The data type of " ,sum , "is", type(sum))
+
+
+a=8
+b=15
+print(a>b)
+
+
+
+num= int(input("Enter a number:"))
+print("You entered", num)
+print("The data type of the input value is", type(num))
